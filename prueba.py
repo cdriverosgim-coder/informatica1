b6ff7c5eb9d0d068ec378673fad3355b18d46517
David Riveros <cdriverosgim@gmail.com>
2025-10-20 14:55:40 -0300

    Create prueba.py

---
+++ prueba.py
@@ -0,0 +1,11 @@
+def hola_mundo() -> str:
+    """
+    Devuelve el saludo 'Hola Git 2025'.
+    """
+    return "Hola Git 2025 --"
+
+
+if __name__ == "__main__":
+    # Implementación/uso de la función
+    mensaje = hola_mundo()
+    print(mensaje)         # Muestra: Hola mundo
